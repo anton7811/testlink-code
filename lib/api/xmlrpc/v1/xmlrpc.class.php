4b43e67e57b873163ebf6cdc6d474849a35c02c2
Anton Putrya <anton7811@gmail.com>
2018-04-26 14:25:47 +0300

    Added an option to change parent test suite for a test case. This has to be reverted for 1.9.17.

--- lib/api/xmlrpc/v1/xmlrpc.class.php
+++ lib/api/xmlrpc/v1/xmlrpc.class.php
@@ -6442,6 +6442,7 @@ protected function createAttachmentTempFile()
     * @param int    $args["executiontype"] - optional - see ... for domain
     * @param int    $args["status'] - optional
     * @param int    $args["estimatedexecduration'] - optional
+    * @param int    $args["testsuiteid"] - optional - update test case parent test suite id
     * @param string $args["user'] - login name used as updater - optional
     *                               if not provided will be set to user that request update
     */
@@ -6560,6 +6561,15 @@ protected function createAttachmentTempFile()
       }    
     }
 
+    if($status_ok)
+    {
+      // if exist proceed with test suite update
+      if ($this->_isParamPresent(self::$testSuiteIDParamName))
+      {
+        $this->tprojectMgr->tree_manager->change_parent($tcaseID,$this->args[self::$testSuiteIDParamName]);
+      }
+    }
+    
     if($status_ok)
     {
       // $this->args['debug'] = $debug_info;
